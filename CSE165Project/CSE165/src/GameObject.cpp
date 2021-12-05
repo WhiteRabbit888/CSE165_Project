0c32af332d643fd845b9784892260ad8d03031e8
lchristenson123 <megal2cas@gmail.com>
2021-12-05 15:16:21 -0800

    added gameobject class and resource manager class

---
+++ CSE165Project/CSE165/src/GameObject.cpp
@@ -0,0 +1,14 @@
+#include "GameObject.h"
+
+#include <glad/glad.h>
+
+GameObject::GameObject()
+    : Position(0.0f, 0.0f), Size(1.0f, 1.0f), Velocity(0.0f), Color(1.0f), Sprite() { }
+
+GameObject::GameObject(glm::vec2 pos, glm::vec2 size, Texture2D sprite, glm::vec3 color, glm::vec2 velocity)
+    : Position(pos), Size(size), Velocity(velocity), Color(color), Sprite(sprite) { }
+
+void GameObject::Draw(Renderer& renderer)
+{
+    renderer.Draw(this->Sprite, this->Position, this->Size, this->Color);
+}
